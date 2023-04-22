42e8f2ac6f14eb873e02b08684372e4ee5abcc67
ManilkaShalinda <manilkar98@gmail.com>
2023-04-22 20:42:00 +0530

    user authentication backend

--- Code/backend/server.js
+++ Code/backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require("./config/db");
 const donationRoutes=require('./routes/donationRoutes');
 const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
@@ -13,9 +14,12 @@ app.get('/',(req,res) =>{
     res.send("API is running..");
 });
 
-app.use('/api/users', userRoutes)
+app.use('/api/users', userRoutes);
 
-app.use('/api/donation', donationRoutes)
+app.use('/api/donation', donationRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
