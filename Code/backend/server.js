7465934bb05a753e6703867ba4f9d8d256dbad2e
ManilkaShalinda <manilkar98@gmail.com>
2023-04-22 16:50:41 +0530

    Implement insert and get

--- Code/backend/server.js
+++ Code/backend/server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
+const donationRoutes=require('./routes/donationRoutes');
 
 
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json());
 
 app.get('/',(req,res) =>{
     res.send("API is running..");
 });
+
+app.use('/api/donation', donationRoutes)
 
 const PORT = process.env.PORT || 5000;
 
