dd6f4c3b441eda824ffa4abfa652d16a64526335
ManilkaShalinda <manilkar98@gmail.com>
2023-05-02 22:07:49 +0530

    fixbackfront

--- Code/backend/server.js
+++ Code/backend/server.js
@@ -1,31 +1,40 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
-const connectDB = require("./config/db");
-const adminRoutes = require("./routes/adminRoutes");
-const userRoutes = require("./routes/userRoutes");
-const submitInnovations = require("./routes/sunmitInnovationRoutes");
-const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const express = require('express');
+const notes = require('./Data/notes')
+const dotenv = require('dotenv');
+const { connect } = require('mongoose');
+const connectDB = require('./config/db')
+const userRoutes = require ('./routes/userRouter');
+const categoryRoutes = require('./routes/categoryRouter');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const path = require('path'); // nodejs module
 
+const app = express();
 dotenv.config();
 connectDB();
 app.use(express.json());
-app.use("*", cors());
 
-app.get("/", (req, res) => {
-	res.send("API is Running");
-});
+//---------deployment--------------//
+// __dirname= path.resolve()
+// if(process.env.NODE_ENV==='production'){
+//  app.use(express.static(path.join(__dirname,'/frontend/build')));
+//  app.get('*',(req,res)=>{
+//   res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+//  })
+// }else{
+//     app.get("/",(res,req)=>{
+//          res.send("API is running...")
+// })
+// }
 
-app.use("/user/admin", adminRoutes);
-app.use("/user",userRoutes)
-app.use("/innovation",submitInnovations);
-
+//---------create routes--------------//
+app.use('/api/users',userRoutes);
+app.use('/api/category', categoryRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server Started on port ${PORT}..`));
 
+//------create port to connect--------------//
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server started on PORT ${PORT} `));
